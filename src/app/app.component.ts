c64ee7697e84d93a36ecfa484e5d68bd2eb60335
Andela1611 <tolic.andela1@gmail.com>
2022-06-14 23:52:08 +0200

    Saving to local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JokesService } from './services/jokes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-jokes';
+  favorites: any = [];
+  joke: any = {};
+  favoriteViewData: any = [];
+  constructor(private jokesService: JokesService) {}
+
+  ngOnInit() {
+    this.jokesService.getRandomJoke().subscribe((joke) => {
+      this.joke = joke;
+    });
+    if (localStorage.getItem('favorites') === null) {
+      localStorage.setItem('favorites', JSON.stringify([]));
+    }
+  }
+
+  newJoke() {
+    this.jokesService.getRandomJoke().subscribe((joke) => {
+      this.joke = joke;
+    });
+  }
+  exists(id: string) {
+    return this.favorites.filter((f: any) => f.id === id).length > 0;
+  }
+  saveJoke() {
+    this.favorites = JSON.parse(localStorage.getItem('favorites') as string);
+    if (!this.exists(this.joke.id) || []) {
+      this.favorites.push(this.joke);
+      localStorage.setItem('favorites', JSON.stringify(this.favorites));
+      this.newJoke();
+    }
+  }
+
+  showFavorites() {
+    this.favoriteViewData = JSON.parse(localStorage.getItem('favorites')!);
+  }
+
+  deleteJoke(id: string) {
+    this.favoriteViewData = this.favoriteViewData.filter(
+      (joke: any) => joke.id !== id
+    );
+    localStorage.setItem('favorites', JSON.stringify(this.favoriteViewData));
+  }
+
+  deleteLocalStorage() {
+    localStorage.removeItem('favorites');
+    window.location.reload();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule} from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; 
 
-import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+
+
+const appRoutes: Routes = [
+ 
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:false}),
   ],
   providers: [],
   bootstrap: [AppComponent]
